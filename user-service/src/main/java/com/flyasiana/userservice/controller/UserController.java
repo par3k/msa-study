47b3bc1bccd23678ee6c9c4af47883c4e985dec8
Alex <hoijae0194@naver.com>
2025-11-06 21:49:53 +0900

    create 회원가입 기능 구현

--- user-service/src/main/java/com/flyasiana/userservice/controller/UserController.java
+++ user-service/src/main/java/com/flyasiana/userservice/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.flyasiana.userservice.controller;
 
+import com.flyasiana.userservice.dto.UserDto;
+import com.flyasiana.userservice.jpa.UserEntity;
 import com.flyasiana.userservice.service.UserService;
 import com.flyasiana.userservice.vo.Greeting;
+import com.flyasiana.userservice.vo.RequestUser;
+import com.flyasiana.userservice.vo.ResponseUser;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -38,5 +44,23 @@ public class UserController {
         log.info("users.welcome ip : {}, {}, {}, {}",
                 request.getRemoteAddr(), request.getRemoteHost() ,request.getRequestURI(), request.getRequestURL());
         return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(user, ResponseUser.class);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity getUsers() {
+        Iterable<UserEntity> userList = userService.getUserByAll();
+        return ResponseEntity.status(HttpStatus.OK).body(userList);
     }
 }
--- user-service/src/main/java/com/flyasiana/userservice/service/UserService.java
+++ user-service/src/main/java/com/flyasiana/userservice/service/UserService.java
@@ -1,7 +1,9 @@
 package com.flyasiana.userservice.service;
 
 import com.flyasiana.userservice.dto.UserDto;
+import com.flyasiana.userservice.jpa.UserEntity;
 
 public interface UserService {
     UserDto createUser(UserDto userDto);
+    public Iterable<UserEntity> getUserByAll();
 }
